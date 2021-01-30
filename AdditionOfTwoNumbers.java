d3e601147995e379439dcbddabd37fa04e9a38ac
theertha-xworkz <theerthakumar.xworkz@gmail.com>
2021-01-30 13:15:26 +0530

    additon of two numbers

---
+++ AdditionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+class AdditionOftwoNumbers
+{
+public static void main(String a[])
+{
+add(89,-17);
+add(78,-10);
+add(23,-5);
+}
+public static void add(int a, int b)
+{
+int c=a+b;
+System.out.println(c);
+}
+}
---
+++ Country.java
@@ -0,0 +1,13 @@
+class Country
+{
+public static void main(String a[])
+{
+develop();
+develop();
+develop();
+}
+public static void develop()
+{
+System.out.println("developing of smart cities");
+}
+}
