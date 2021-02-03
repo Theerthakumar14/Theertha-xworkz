f91f9ddf6fe0cdae05d1d9c3ebd3d462078d27d2
theertha-xworkz <theerthakumar.xworkz@gmail.com>
2021-02-03 21:16:18 +0530

    Adding method file

---
+++ Phone.java
@@ -0,0 +1,18 @@
+class Phone
+{
+public static void main(String a[])
+{
+ring();
+ring ("massy");
+
+}
+public static void ring()
+{
+System.out.println("oneplus and iphone ringing");
+}
+public static void ring(String songs)
+{
+System.out.println("oneplus and iphone ringing"   + songs);
+}
+
+}
